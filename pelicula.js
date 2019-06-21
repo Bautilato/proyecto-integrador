50188952acb92352c0f6099dbee562ba2043d3db
Tomas <tomasbongarra00@gmail.com>
2019-06-21 12:39:00 -0300

    fagetahh

--- pelicula.js
+++ pelicula.js
@@ -188,27 +188,27 @@ for (var i = 0; i < 5; i++) {
 var arrayDePelisFavoritas = []
 
 
-function agregarFavoritos(id) {
+function agregarFavoritos(idDePeli) {
  alert("me clickearon!!!")
 
- if (arrayDePelisFavoritas.indexOf(id)===-1) {
+ if (arrayDePelisFavoritas.indexOf(idDePeli)===-1) {
    // EN ESTE CASO NO ES FAVORITA
    // pusheo el id dentro del array
-   arrayDePelisFavoritas.push(id)
+   arrayDePelisFavoritas.push(idDePeli)
    // guardo en session el array, como es un objeto debo transformarlo a STRING
    window.sessionStorage.setItem("favorita",JSON.stringify(arrayDePelisFavoritas))
 
  }
  else {
    // ESTA PELI YA ES FAVORITA
-   console.log(arrayDePelisFavoritas.indexOf(id));
+   console.log(arrayDePelisFavoritas.indexOf(idDePeli));
    // la saco del array
-   arrayDePelisFavoritas.splice(arrayDePelisFavoritas.indexOf(id),1)
+   arrayDePelisFavoritas.splice(arrayDePelisFavoritas.indexOf(idDePeli),1)
    console.log(arrayDePelisFavoritas);
    // reemplazo el array que tenia la peli como favorita, por el array que ya no la tiene
    window.sessionStorage.setItem("favorita",JSON.stringify(arrayDePelisFavoritas))
  }
- console.log(id);
+ console.log(idDePeli);
   console.log(JSON.parse(window.sessionStorage.getItem("favorita")));
 
 }
